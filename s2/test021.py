6740e9e89046938934566c0a0ed96119f49d1204
VK <jinzhenqian@outlook.com>
2024-01-09 20:41:35 +0800

    图像的翻转和旋转

--- s2/test021.py
+++ s2/test021.py
@@ -5,7 +5,10 @@ import numpy as np
 
 robot = cv2.imread('robot-2.jpg')
 h,w,ch=robot.shape
-M=np.float32([[1,0,100],[0,1,300]])
+# M=np.float32([[1,0,100],[0,1,300]])
+# 通过api计算M，中心点是x，y，也就是宽、高
+M=cv2.getRotationMatrix2D((w/2,h/2),15,0.3)
+
 robot2=cv2.warpAffine(robot,M,(w,h) )
 
 cv2.imshow('robot',robot)
